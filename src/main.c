aa027598de911c8ba55e0674681b5dedf1b9fe8a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-03 23:47:15 +0200

    Display a window

--- src/main.c
+++ src/main.c
@@ -1,10 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include <SDL2/SDL.h>
 
 int main()
 {
-    if(0 != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO))
+    if(0 != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        fprintf(stderr, "Erreur SDL_Init : %s\n", SDL_GetError());
         return EXIT_FAILURE;
+    }
 
+    SDL_Window *window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              640, 480, SDL_WINDOW_SHOWN);
+
+    if(NULL == window)
+    {
+        fprintf(stderr, "Erreur SDL_CreateWindow : %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Event event;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return EXIT_SUCCESS;
