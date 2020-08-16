613a4efc4c1c080531725c1b5db5e4e1b76788c1
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-16 12:26:09 +0200

    Add script to measure polynome sin approx perfs

---
+++ scripts/poly_sin_cos_perf.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+
+#include "../include/get_time_ms.h"
+#include "../include/poly_sin_cos.h"
+
+volatile double s, a;
+
+int main() {
+    double start_time;
+    static const int nb_tries = 100000000;
+
+    a = 0.5;
+
+    start_time = get_time_ms();
+    for(int i = 0; i < nb_tries; ++i) {
+        s = sin(a);
+    }
+
+    printf("math.h sin (ms): %f\n", get_time_ms() - start_time);
+
+    start_time = get_time_ms();
+    for(int i = 0; i < nb_tries; ++i) {
+        s = poly_sin(a);
+    }
+
+    printf("polynome sin (ms): %f\n", get_time_ms() - start_time);
+}
