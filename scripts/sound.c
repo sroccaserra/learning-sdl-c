cf96dffb60160a070e93ca11217fe128598c67e6
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-08-30 16:15:59 +0200

    Extract sine, saw and pulse functions in sound ex.

--- scripts/sound.c
+++ scripts/sound.c
@@ -10,70 +10,20 @@
 typedef int16_t T;
 static const uint8_t BIT_DEPTH = 8*sizeof(T);
 
-static T min_value() {
-    return -pow(2, BIT_DEPTH-1);
-}
+void audio_callback(void* userdata, Uint8* byte_stream, int len);
+static T min_value();
+static T max_value();
 
-static T max_value() {
-    return pow(2, BIT_DEPTH-1)-1;
-}
+double sine_wave(const double frequency, const double t);
+double saw_wave(const double frequency, const double t);
+double pulse_wave(const double frequency, const double pulse_width, const double t);
 
 typedef struct {
     double attack;
     double release;
 } Envelope;
 
-double apply_envelope(const Envelope *envelope, double t, double value) {
-    assert(0 <= t);
-
-    if (0 < envelope->attack && t <= envelope->attack) {
-        return value*t/envelope->attack;
-    }
-
-    const double duration = envelope->attack + envelope->release;
-
-    if (0 == envelope->release || duration <= t) {
-        return 0;
-    }
-
-    const double k = 1 + (envelope->attack-t)/envelope->release;
-
-    return value*k;
-}
-
-void audio_callback(void* userdata, Uint8* byte_stream, int len) {
-    (void)userdata; // unused variable
-    static const double SINE_FREQ = 220.;
-    static const double SAW_FREQ = 1.001*3*220.;
-    static const double PULSE_FREQ = 0.999*5*55.;
-    static const Envelope envelope = {
-        .attack = 0.01,
-        .release = 0.8,
-    };
-    static int start_frame = 0;
-
-    const int nb_frames = len/(NB_CHANNELS*sizeof(T));
-    T *stream = (T *)byte_stream;
-
-    for (int i = 0; i < nb_frames; ++i) {
-        const double t = (start_frame + i)/SAMPLE_RATE;
-
-        const double sine = sin(t*2*M_PI*SINE_FREQ);
-        const double saw = 2*SAW_FREQ*fmod(t, 1/SAW_FREQ) - 1;
-        const double pulse = (fmod(t, 1/PULSE_FREQ) > .25/PULSE_FREQ) ? 1 : 0;
-
-        const double x = apply_envelope(&envelope, t, 0.05*saw + 0.2*sine + 0.04*pulse);
-
-        const T value = max_value()*x;
-        assert(min_value() <= value && value <= max_value());
-
-        for (int j = 0; j < NB_CHANNELS; ++j) {
-            stream[NB_CHANNELS*i + j] = value;
-        }
-    }
-
-    start_frame += nb_frames;
-}
+double apply_envelope(const Envelope *envelope, double t, double value);
 
 int main() {
     if(0 != SDL_Init(SDL_INIT_AUDIO)) {
@@ -107,4 +57,77 @@ int main() {
     SDL_Quit();
 
     return EXIT_SUCCESS;
+}
+
+void audio_callback(void* userdata, Uint8* byte_stream, int len) {
+    (void)userdata; // unused variable
+    static const double SINE_FREQ = 220.;
+    static const double SAW_FREQ = 1.001*3*220.;
+    static const double PULSE_FREQ = 0.999*5*55.;
+    static const Envelope envelope = {
+        .attack = 0.01,
+        .release = 0.8,
+    };
+    static int start_frame = 0;
+
+    const int nb_frames = len/(NB_CHANNELS*sizeof(T));
+    T *stream = (T *)byte_stream;
+
+    for (int i = 0; i < nb_frames; ++i) {
+        const double t = (start_frame + i)/SAMPLE_RATE;
+
+        const double sine = sine_wave(SINE_FREQ, t);
+        const double saw = saw_wave(SAW_FREQ, t);
+        const double pulse = pulse_wave(PULSE_FREQ, .25, t);
+
+        const double x = apply_envelope(&envelope, t, 0.05*saw + 0.2*sine + 0.04*pulse);
+
+        const T value = max_value()*x;
+        assert(min_value() <= value && value <= max_value());
+
+        for (int j = 0; j < NB_CHANNELS; ++j) {
+            stream[NB_CHANNELS*i + j] = value;
+        }
+    }
+
+    start_frame += nb_frames;
+}
+
+static T min_value() {
+    return -pow(2, BIT_DEPTH-1);
+}
+
+static T max_value() {
+    return pow(2, BIT_DEPTH-1)-1;
+}
+
+double sine_wave(const double frequency, const double t) {
+    static const double TAU = 2*M_PI;
+    return sin(t*TAU*frequency);
+}
+
+double saw_wave(const double frequency, const double t) {
+    return 2*frequency*fmod(t, 1/frequency) - 1;
+}
+
+double pulse_wave(const double frequency, const double pulse_width, const double t) {
+    return (fmod(t, 1/frequency) > pulse_width/frequency) ? 1 : 0;
+}
+
+double apply_envelope(const Envelope *envelope, double t, double value) {
+    assert(0 <= t);
+
+    if (0 < envelope->attack && t <= envelope->attack) {
+        return value*t/envelope->attack;
+    }
+
+    const double duration = envelope->attack + envelope->release;
+
+    if (0 == envelope->release || duration <= t) {
+        return 0;
+    }
+
+    const double k = 1 + (envelope->attack-t)/envelope->release;
+
+    return value*k;
 }
